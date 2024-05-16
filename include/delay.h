bc0a79c62bfa386e0a654a37c0c556757b6abe74
hiifong <i@hiif.ong>
2024-05-17 00:39:53 +0800

    init

---
+++ include/delay.h
@@ -0,0 +1,12 @@
+//
+// Created by hiifong on 2024/5/16.
+//
+
+#ifndef __DELAY_H
+#define __DELAY_H
+
+void Delay_us(uint32_t us);
+void Delay_ms(uint32_t ms);
+void Delay_s(uint32_t s);
+
+#endif
---
+++ include/main.h
@@ -0,0 +1,10 @@
+//
+// Created by hiifong on 2024/5/16.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "stm32f10x.h"
+
+#endif //MAIN_H
